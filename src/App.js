9e273e1a81758628522530b159ec213830d3055f
mrprofessor <kar.rudra008@gmail.com>
2019-09-02 04:38:18 +0530

    Create react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>
+          #art
+        </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
