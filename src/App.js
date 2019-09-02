ab833d8b22aec8e5c4961669c898f5962db6163d
schadokar <shubhamchadokar04@gmail.com>
2019-09-02 23:47:12 +0530

    rgh: generate-hash component complete and integrated in left

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { Button } from 'evergreen-ui'
+import React from "react";
+import { Button } from "evergreen-ui";
 import Splitter from "./components/Splitter/Splitter.js";
+import Hash from "./components/Hash/gen-hash.js";
 
 function App() {
   return (
-    <div >
-        <Button>I am using Evergreen!</Button>
-        <Splitter 
-          leftPane={<p>left</p>}
-          rightPane={<p>right</p>} 
-        />
+    <div>
+      <Button>I am using Evergreen!</Button>
+      <Splitter
+        leftPane={
+          <p>
+            {" "}
+            <Hash></Hash>
+          </p>
+        }
+        rightPane={<p>right</p>}
+      />
     </div>
   );
 }
