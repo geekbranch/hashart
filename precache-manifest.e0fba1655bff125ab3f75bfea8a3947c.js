cf69e435086b873201952f4ee12a03b50b68f44c
mrprofessor <kar.rudra008@gmail.com>
2019-09-03 05:28:50 +0530

    Updates

---
+++ precache-manifest.e0fba1655bff125ab3f75bfea8a3947c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557415dcd36131cfb7c65fe2d6578aeb",
+    "url": "/hashart/index.html"
+  },
+  {
+    "revision": "d2e29c62e1bcd300f3f4",
+    "url": "/hashart/static/css/main.f2738e2f.chunk.css"
+  },
+  {
+    "revision": "6ef8f78518b3db40e001",
+    "url": "/hashart/static/js/2.8d2fd193.chunk.js"
+  },
+  {
+    "revision": "d2e29c62e1bcd300f3f4",
+    "url": "/hashart/static/js/main.5a727188.chunk.js"
+  },
+  {
+    "revision": "656e74724950705c4f0c",
+    "url": "/hashart/static/js/runtime~main.74a82130.js"
+  }
+]);
